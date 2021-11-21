89c950b0667e40828c8fb39d86a7dc5037bb17a0
unknown <yoonhero06@naver.com>
2021-11-21 09:43:37 +0900

    feat: Make Block

--- main.py
+++ main.py
@@ -4,4 +4,25 @@ from ursina.prefabs.first_person_controller import FirstPersonController
 
 app = Ursina()
 
-app.run()
+window.fps_counter.enabled = False
+window.exit_button.visible = False
+
+class Voxel(Button):
+    def __init__(self, position=(0, 0, 0), texture="brick"):
+        super().__init__(
+            parent=scene,
+            position=position,
+            origin_y=0.5,
+            model="cube",
+            texture=texture,
+            color=color.color(0, 0, random.uniform(0.9, 1.0)),
+            scale=1.0
+        )
+        
+for z in range(20):
+    for x in range(20):
+        voxel = Voxel(position=(x, 0,z))
+
+player = FirstPersonController()
+        
+app.run()   
